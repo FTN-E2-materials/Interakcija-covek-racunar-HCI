80d76491092b5da8a76f0ffc4e4ffc04d6367141
vlaksi <daksrapsody@gmail.com>
2020-05-02 14:20:21 +0200

    Pocetni deo aplikacije kroz XAML i Material Design

--- projekat/Vaksi/HealthClinic/HealthClinic/MainWindow.xaml.cs
+++ projekat/Vaksi/HealthClinic/HealthClinic/MainWindow.xaml.cs
@@ -24,5 +24,37 @@ namespace HealthClinic
         {
             InitializeComponent();
         }
+
+        private void promenaTaba(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);                                      // Uzimamo index taba koji smo kliknuli
+
+            GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);                     // Pomeranje ikonice koja oznacava na kom smo tabu trenutno
+
+            switch (index)                                                                      // U zavisnosti od taba, prikazujemo drugacije telo nase aplikacije.
+            {                                                                                   // Trenutno je to samo promena boje, ali u daljim verzijama cemo tu stavljati 
+                case 0:                                                                         // deo koji predstavlja deo tog dela app.
+                    GridMain.Background = Brushes.Aquamarine;
+                    break;
+                case 1:
+                    GridMain.Background = Brushes.Beige;
+                    break;
+                case 2:
+                    GridMain.Background = Brushes.CadetBlue;
+                    break;
+                case 3:
+                    GridMain.Background = Brushes.DarkBlue;
+                    break;
+                case 4:
+                    GridMain.Background = Brushes.Firebrick;
+                    break;
+                case 5:
+                    GridMain.Background = Brushes.Gainsboro;
+                    break;
+                case 6:
+                    GridMain.Background = Brushes.HotPink;
+                    break;
+            }
+        }
     }
 }
