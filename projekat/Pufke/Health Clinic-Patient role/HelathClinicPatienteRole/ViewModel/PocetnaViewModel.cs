588baba970963b43f40df0fc48b408f4649bf5c4
Pufke <pufke1998@gmail.com>
2020-05-10 14:46:04 +0200

    Sitne dorade na projektu

--- projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/PocetnaViewModel.cs
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/PocetnaViewModel.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HelathClinicPatienteRole.ViewModel
 {
+    
     class PocetnaViewModel
     {
+        
     }
+
 }
