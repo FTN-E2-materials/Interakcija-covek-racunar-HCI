cd3e109b47d44d48e3e9446fd1616f33546c2a8e
Pufke <pufke1998@gmail.com>
2020-05-11 23:13:24 +0200

    Implementiran Scheduler u Propisanim terapijama

---
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/Model/MedicinskiKarton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelathClinicPatienteRole.Model
+{
+    class MedicinskiKarton : INotifyPropertyChanged
+    {
+        private int userId;
+        private string firstAndLastName;
+        private string parentName;
+        private string dateOfBirth;
+        private string jmbg;
+        private string address;
+        private string phoneNumber;
+        private string healthInsuranceNumber;
+        private string healthInsuranceCarrier; //Nosilac zdravstvenog osiguranja
+
+        public int UserId { get; set; }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
--- projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/KartonPatientViewModel.cs
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/KartonPatientViewModel.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using HelathClinicPatienteRole.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HelathClinicPatienteRole.ViewModel
 {
     class KartonPatientViewModel
     {
+        private IList<User> _UsersList;
+
+        public KartonPatientViewModel()
+        {
+            _UsersList = new List<User>
+            {
+                new User{UserId = 1,FirstName="Marko",LastName="Markovic", Jmbg="0208998500079", PhoneNumber="0602545687",Email="marko@gmail.com"}
+            };
+        }
+
+        public IList<User> Users
+        {
+            get { return _UsersList; }
+            set { _UsersList = value; }
+        }
+
+        private ICommand mUpdater;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (mUpdater == null)
+                    mUpdater = new Updater();
+                return mUpdater;
+            }
+            set
+            {
+                mUpdater = value;
+            }
+        }
+
+        private class Updater : ICommand
+        {
+            #region ICommand Members  
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+
+            }
+
+            #endregion
+        }
     }
 }
