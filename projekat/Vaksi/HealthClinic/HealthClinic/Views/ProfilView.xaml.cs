3a273ec00805509571d94b657355ce7a6aeab1b8
vlaksi <daksrapsody@gmail.com>
2020-05-13 00:19:00 +0200

    Cartesian dijagram

--- projekat/Vaksi/HealthClinic/HealthClinic/Views/ProfilView.xaml.cs
+++ projekat/Vaksi/HealthClinic/HealthClinic/Views/ProfilView.xaml.cs
@@ -27,9 +27,10 @@ namespace HealthClinic.Views
         {
             InitializeComponent();
             this.PieChart();
+            this.Cartesian();
         }
 
-        #region Grafikon 
+        #region Grafikon PieChart
         public Func<ChartPoint, string> PointLabel { get; set; }
 
         public void PieChart()
@@ -46,9 +47,46 @@ namespace HealthClinic.Views
 
         #endregion
 
-        private void Flipper_IsFlippedChanged(object sender, RoutedPropertyChangedEventArgs<bool> e)
-        {
+        #region Grafikon Cartesian
 
+        public Func<double, string> yFormatter { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+
+        public void Cartesian()
+        {
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title="Decije", Values = new  ChartValues<double>{100,200,100,200 }
+                },
+                new LineSeries
+                {
+                    Title="Hirurgija", Values = new  ChartValues<double>{1000,1200,1300,1900 }
+                },
+                new LineSeries
+                {
+                    Title="Oporavak", Values = new  ChartValues<double>{200,600,100,200 }
+                },
+                new LineSeries
+                {
+                    Title="Psihijatrija", Values = new  ChartValues<double>{10,3000,10,20 }
+                }
+
+            };
+
+            //Labels = new[] { "q1", "Q2", "Q3", "Q4" };
+            //yFormatter = value => value.ToString("C");      //TODO: Proveriti sta je ovo "C" u toString
+
+            //SeriesCollection.Add(new LineSeries
+            //{
+            //    Title = "Nesto bzv",Values = new ChartValues<double> { 10,11,12}
+            //});
+
+            //SeriesCollection[3].Values.Add(1000d);
         }
+
+        #endregion
     }
 }
