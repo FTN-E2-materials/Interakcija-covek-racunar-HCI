b702c61d4577e063bbe98b82cfd4c967cc7ae353
vlaksi <daksrapsody@gmail.com>
2020-05-24 20:48:53 +0200

    Uspesno prebacen CodeBehind za Video deo u ViewModel deo

--- projekat/Vaksi/HealthClinic/HealthClinic/Views/AboutView.xaml.cs
+++ projekat/Vaksi/HealthClinic/HealthClinic/Views/AboutView.xaml.cs
@@ -21,44 +21,9 @@ namespace HealthClinic.Views
     /// </summary>
     public partial class AboutView : UserControl
     {
-        DispatcherTimer timer;
-
         public AboutView()
         {
             InitializeComponent();
-
-            // Omogucenje promene value slajdera vremena videa nakon nekoliko milisekundi klipa.
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(500);
-            timer.Tick += Timer_Tick;
-
-            KontrolaKlipa.Volume =(double)SlajderZvuka.Value;
         }
-
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            SlajderVremenaVidea.Value = KontrolaKlipa.Position.TotalSeconds;
-        }
-
-        private void SlajderVremenaVidea_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            KontrolaKlipa.Position = TimeSpan.FromSeconds(SlajderVremenaVidea.Value);
-        }
-
-        private void SlajderZvuka_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            KontrolaKlipa.Volume = (double)SlajderZvuka.Value;
-        }
-
-        // Otvaranjem klipa pocinje tiker koji pomera slajder vremena u videu.
-        private void KontrolaKlipa_MediaOpened(object sender, RoutedEventArgs e)
-        {
-            TimeSpan ts = KontrolaKlipa.NaturalDuration.TimeSpan;
-            SlajderVremenaVidea.Maximum = ts.TotalSeconds;
-            timer.Start();
-        }
-
-
     }
 }
