eafdab23ccbf44ed245b8a60373b5a2d71c51781
vlaksi <daksrapsody@gmail.com>
2020-05-26 00:18:03 +0200

    Bajndovanje podataka u dijalog iz tabele [zaposleni]

--- projekat/Vaksi/HealthClinic/HealthClinic/ViewModels/ZaposleniViewModel.cs
+++ projekat/Vaksi/HealthClinic/HealthClinic/ViewModels/ZaposleniViewModel.cs
@@ -11,21 +11,36 @@ using System.Threading.Tasks;
 
 namespace HealthClinic.ViewModels
 {
-    public class ZaposleniViewModel
-    {
-        public ObservableCollection<Zaposlen> Zaposleni { get; set; }
-        
+    public class ZaposleniViewModel:ObservableObject
+    {   
         public ZaposleniViewModel()
         {
-            this.PieChart();                // init piecharta
+            PieChart();                // init piecharta
 
             ucitavanjePodatakaUTabelu();
 
             DodajZaposlenogCommand = new RelayCommand(PrikaziDijalogDodavanjaZaposlenog);
             IzmeniZaposlenogCommand = new RelayCommand(PrikaziDijalogIzmeneZaposlenog);
             GenerisiIzvestajZaposlenogCommand = new RelayCommand(PrikaziDijalogGenerisanjaIzvestaja);
+            //SelektovaniZaposleni.
         }
 
+        #region Selektovani zaposlen
+
+        private Zaposlen _selektovaniZaposleni;
+
+
+        // Bajndujem na SelectedItem u tabeli i dalje radim sa njim sta hocu
+        // mogu ga dalje prikazivati
+        // a moze se i proslediti u dijalog
+        // tako sto se .DatContext tog dijalog postavi na this
+        public Zaposlen SelektovaniZaposleni
+        {
+            get { return _selektovaniZaposleni; }
+            set { _selektovaniZaposleni = value; OnPropertyChanged("SelektovaniZaposleni"); }
+        }
+
+        #endregion
 
         #region Komande
 
@@ -50,11 +65,13 @@ namespace HealthClinic.ViewModels
         public void PrikaziDijalogIzmeneZaposlenog(object obj)
         {
             var dijalog = new IzmeniZaposlenogDijalog();
-            dijalog.ShowDialog();
+            dijalog.DataContext = this;             // kako bi prebacio podatke iz ovog prozora u dijalog
+            dijalog.ShowDialog();                   // podesavam da i dijalog moze upravljati istim podacima
         }
 
         #endregion
 
+        #region Tabela
         private void ucitavanjePodatakaUTabelu()
         {
             //Tabela - popunjavanje
@@ -70,7 +87,9 @@ namespace HealthClinic.ViewModels
             Zaposleni.Add(new Zaposlen() { Ime = "Boban", Prezime = "Jokic", Struka = "Pedijatar", Sifra = "*****", RadniKalendar = "slobodan", BrojOperacijaOveNedelje = "9" });
             Zaposleni.Add(new Zaposlen() { Ime = "Nikola", Prezime = "Marjanovic", Struka = "Doktor opste prakse", Sifra = "*****", RadniKalendar = "popunjen", BrojOperacijaOveNedelje = "12" });
         }
+        public ObservableCollection<Zaposlen> Zaposleni { get; set; }
 
+        #endregion
 
         #region Grafikon
         public Func<ChartPoint, string> PointLabel { get; set; }
