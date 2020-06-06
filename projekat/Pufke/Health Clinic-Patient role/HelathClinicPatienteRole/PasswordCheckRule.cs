346b767e2b3e5db2e2d14343a26da8e5aaccdcda
Pufke <pufke1998@gmail.com>
2020-06-06 22:46:04 +0200

    Implementirana online validacija passworda prilikom logina, uslov minimum 8 karaktera

---
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/PasswordCheckRule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HelathClinicPatienteRole
+{
+    class PasswordCheckRule : ValidationRule
+    {
+        public int MinimumCharacters { get; set; }
+   
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string charString = value as string;
+
+        
+            if (charString.Length < MinimumCharacters)
+                return new ValidationResult(false, $"Šifra mora da sadrži minimun {MinimumCharacters} karaktera!.");
+
+            return new ValidationResult(true, null);
+        }
+
+
+    }
+}
--- projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/LoginViewModel.cs
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/LoginViewModel.cs
@@ -10,12 +10,22 @@ namespace HelathClinicPatienteRole.ViewModel
     class LoginViewModel : ObservableObject
     {
         private string _username;
+        private string _password;
         public string Username
         {
             get { return _username; }
             set
             {
                 OnPropertyChanged(ref _username, value);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                OnPropertyChanged(ref _password, value);
             }
         }
     }
