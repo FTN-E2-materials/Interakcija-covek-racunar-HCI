2729620dbef2341e19efab81bfdfb95fa33f2281
Pufke <pufke1998@gmail.com>
2020-06-06 22:34:51 +0200

    Implementirana validacija JMBG-a do kraja, potrebno uneti 13 cifara, i prihvata samo cifre

--- projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/MinimumCharacterRule.cs
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/MinimumCharacterRule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
@@ -11,17 +12,25 @@ namespace HelathClinicPatienteRole
     public class MinimumCharacterRule : ValidationRule
     {
         public int MinimumCharacters { get; set; }
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex samo za brojeve
 
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             string charString = value as string;
 
-            if (charString.Length < MinimumCharacters)
-                return new ValidationResult(false, $"Minimum {MinimumCharacters} karaktera!.");
+            if (IsTextNumber(charString))
+            {
+                return new ValidationResult(false, $"Samo cifre!");
+            }
+            if (!(charString.Length == MinimumCharacters))
+                return new ValidationResult(false, $"JMBG sadrzi tacno {MinimumCharacters} cifara!.");
 
             return new ValidationResult(true, null);
         }
 
-       
+        private static bool IsTextNumber(string text)
+        {
+            return _regex.IsMatch(text);
+        }
     }
 }
