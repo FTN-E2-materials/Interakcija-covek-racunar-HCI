0dc7a3942d829ff16e3426e98ab3aacc88e011b6
vlaksi <daksrapsody@gmail.com>
2020-06-07 03:46:15 +0200

    Validacija za lekove & prostorije + NumberRules(restrikcija polja samo na brojeve)

---
+++ projekat/Vaksi/HealthClinic/HealthClinic/Utilities/Validations/NumberRule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HealthClinic.Utilities.Validations
+{
+    /// <summary>
+    /// Restrikcija koriscena samo brojeva
+    /// </summary>
+    public class NumberRule : ValidationRule
+    {
+        /// <summary>
+        /// Jedan ili vise brojeva samo
+        /// </summary>
+        private static readonly Regex _regexForNumber = new Regex("^[0-9]+$");
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string charString = value as string;
+
+            if (!IsOnlyNumber(charString))
+            {
+                return new ValidationResult(false, $"U ovo polje je dozvoljeno samo unositi brojeve");
+            }
+
+            return new ValidationResult(true, null);
+        }
+
+        private static bool IsOnlyNumber(string text)
+        {
+            return _regexForNumber.IsMatch(text);
+        }
+    }
+}
