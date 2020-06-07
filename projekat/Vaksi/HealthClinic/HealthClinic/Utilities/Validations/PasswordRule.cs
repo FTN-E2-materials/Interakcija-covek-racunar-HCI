1bdeb197cc303b3e2c4947bbf4f5202e400c2dbd
vlaksi <daksrapsody@gmail.com>
2020-06-07 03:02:43 +0200

    Validacija lozinke zaposlenog

---
+++ projekat/Vaksi/HealthClinic/HealthClinic/Utilities/Validations/PasswordRule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HealthClinic.Utilities.Validations
+{
+    /// <summary>
+    /// Custom klasa za odredjivanje validne lozinke
+    /// </summary>
+    public class PasswordRule : ValidationRule
+    {
+        /// <summary>
+        /// Vazenje: Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+        /// link: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+        /// </summary>
+        private static readonly Regex _regexForPassword = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string charString = value as string;
+
+            if (!IsValidPassword(charString))
+            {
+                return new ValidationResult(false, $"Minimalno 8 slova, jedno malo i jedno veliko slovo, bar jedan broj.");
+            }
+
+            return new ValidationResult(true, null);
+        }
+
+
+        private static bool IsValidPassword(string text)
+        {
+            return _regexForPassword.IsMatch(text);
+        }
+    }
+}
