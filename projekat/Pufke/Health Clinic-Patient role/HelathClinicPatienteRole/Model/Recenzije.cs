f1d4fc1018fe7c7c452e39189bf6055e2d32f809
Pufke <pufke1998@gmail.com>
2020-06-08 03:49:49 +0200

    Implementiran novi dizajn Recenzije APP, i omoguceno dodavanje recenzije i automatski prikaz iste u tabeli svih recenzija

---
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/Model/Recenzije.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelathClinicPatienteRole.Model
+{
+    class Recenzije : INotifyPropertyChanged
+    {
+        private string korisnik;
+        private string recenzija;
+
+        public string Korisnik
+        {
+            get { return korisnik; }
+            set
+            {
+                korisnik = value;
+                OnPropertyChanged("Korisnik");
+            }
+        }
+
+        public string Recenzija
+        {
+            get { return recenzija; }
+            set
+            {
+                recenzija = value;
+                OnPropertyChanged("Recenzija");
+            }
+        }
+
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+       
+        #endregion
+    }
+}
--- projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/RecenzijaAppPatientViewModel.cs
+++ projekat/Pufke/Health Clinic-Patient role/HelathClinicPatienteRole/ViewModel/RecenzijaAppPatientViewModel.cs
@@ -1,12 +1,135 @@
-﻿using System;
+﻿using HelathClinicPatienteRole.Model;
+using HelathClinicPatienteRole.ViewModel.Commands;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace HelathClinicPatienteRole.ViewModel
 {
-    class RecenzijaAppPatientViewModel
+    class RecenzijaAppPatientViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<Recenzije> _RecenzijeList;
+        private string noviKomentar;
+
+        public RecenzijaAppPatientViewModel()
+        {
+           OstaviRecenzijuCommand = new RelayCommand(OstaviRecenziju);
+
+            _RecenzijeList = new ObservableCollection<Recenzije>
+            {
+                new Recenzije{Korisnik = "Pero Mikić",Recenzija="Veoma dobra aplikacija, vrlo mi olakšava zakazivanje pregleda kao i pregled svih ostalih informacija koje me zanimaju. Blog vam je odličan!" },
+                new Recenzije{Korisnik = "Pero Mikić",Recenzija="Veoma dobra aplikacija, vrlo mi olakšava zakazivanje pregleda kao i pregled svih ostalih informacija koje me zanimaju. Blog vam je odličan!" },
+                new Recenzije{Korisnik = "Pero Mikić",Recenzija="Veoma dobra aplikacija, vrlo mi olakšava zakazivanje pregleda kao i pregled svih ostalih informacija koje me zanimaju. Blog vam je odličan!" },
+            };
+        }
+
+        #region Ostavi recenziju
+
+        public RelayCommand OstaviRecenzijuCommand { get; private set; }
+
+        public void OstaviRecenziju(object obj)
+        {
+            if (NoviKomentar == null)
+            {
+                MessageBox.Show("Morate uneti vaš komentar! ");
+                return;
+            }
+
+            Recenzije novaRecenzija = new Recenzije();
+            novaRecenzija.Korisnik = "Marko Marković";
+            novaRecenzija.Recenzija = NoviKomentar;
+
+            
+            Recenzije.Add(novaRecenzija);
+           
+
+            MessageBox.Show("Uspesno ste ostavili recenziju ");
+
+        }
+
+        #endregion
+
+        # region ICommand
+        private ICommand mUpdater;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (mUpdater == null)
+                    mUpdater = new Updater();
+                return mUpdater;
+            }
+            set
+            {
+                mUpdater = value;
+            }
+        }
+
+        private class Updater : ICommand
+        {
+            #region ICommand Members  
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+
+            }
+
+            #endregion
+        }
+        #endregion
+   
+
+        public string NoviKomentar
+        {
+            get
+            {
+                return noviKomentar;
+            }
+            set { noviKomentar = value; }
+        }
+
+        public ObservableCollection<Recenzije> Recenzije
+        {
+            get
+            {
+                return _RecenzijeList;
+            }
+            set { _RecenzijeList = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        #region Selektovana recenzija
+
+        private Recenzije _selektovanaRecenzija;
+
+        public Recenzije SelektovanaRecenzija
+        {
+            get { return _selektovanaRecenzija; }
+            set { _selektovanaRecenzija = value; OnPropertyChanged("SelektovanaRecenzija"); }
+        }
+
+        #endregion
+
     }
 }
